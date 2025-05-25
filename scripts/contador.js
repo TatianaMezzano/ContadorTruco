0278cc5573d189859ee017da91346c4b98545424
Tatiana Mezzano <134722879+TatianaMezzano@users.noreply.github.com>
2025-05-25 14:45:04 -0300

    ¨¨

--- scripts/contador.js
+++ scripts/contador.js
@@ -32,17 +32,35 @@ export class Contador{
     }
 
     actualizarCuenta(){
-        const fosforosActuales = this.containerElement.querySelectorAll("img");        
+        const separadoresActuales = this.containerElement.querySelectorAll(".separador");        
+        const gruposActuales = this.containerElement.querySelectorAll(".grupo");
+        if (gruposActuales.length > 0){
+            gruposActuales.forEach(grupo => this.cuentaElement.removeChild(grupo))
+            separadoresActuales.forEach(separador => this.cuentaElement.removeChild(separador))
 
-        if (fosforosActuales){
-            fosforosActuales.forEach(fosforo => this.cuentaElement.removeChild(fosforo))
         }
 
+        let grupoActual;
         for (let i = 0; i<this.numero;i++) {
+            if (i%5 === 0){
+                grupoActual = document.createElement("div");
+                grupoActual.classList.add("grupo");
+                
+
+                if (i%15 === 0 && i!== 0){
+                    const separador = document.createElement("div");
+                    separador.classList.add("separador");
+                    this.cuentaElement.appendChild(separador);
+                }
+
+                this.cuentaElement.appendChild(grupoActual);
+            }
+            
             const nuevoFosforo = document.createElement("img");
             nuevoFosforo.src = "../img/fosforo.png";
             nuevoFosforo.classList.add("fosforo"+(i%5+1));
-            this.cuentaElement.appendChild(nuevoFosforo);
+            grupoActual.appendChild(nuevoFosforo);
+            
             
         }
     }
