3f93fdb4e2c2f7bc37c423921299316ec82c46d5
Tatiana Mezzano <134722879+TatianaMezzano@users.noreply.github.com>
2025-05-25 13:58:34 -0300

    ¨fosforos

--- scripts/contador.js
+++ scripts/contador.js
@@ -1,26 +1,49 @@
 export class Contador{
     numero = 0;
     nombre;
+    cuentaElement;
+    containerElement;
 
     constructor(name, containerElement, cuentaInicial = 0){
         this.nombre = name;
         this.numero = cuentaInicial;
+        this.containerElement = containerElement;
         containerElement.querySelector("h2").innerText = name;
         containerElement.querySelector(".agregar").addEventListener("click",()=>this.agregar());
         containerElement.querySelector(".restar").addEventListener("click",()=>this.restar());
+        this.cuentaElement = containerElement.querySelector(".cuenta");
     }
 
     agregar(cantidad = 1){
         this.numero += cantidad;
         console.log(this.numero);
+        this.actualizarCuenta();
     }
 
     restar(cantidad = 1){
         this.numero = Math.max(0,this.numero - cantidad);
         console.log(this.numero);
+        this.actualizarCuenta();
     }
 
     reset(){
         this.numero = 0;
+        this.actualizarCuenta();
+    }
+
+    actualizarCuenta(){
+        const fosforosActuales = this.containerElement.querySelectorAll("img");        
+
+        if (fosforosActuales){
+            fosforosActuales.forEach(fosforo => this.cuentaElement.removeChild(fosforo))
+        }
+
+        for (let i = 0; i<this.numero;i++) {
+            const nuevoFosforo = document.createElement("img");
+            nuevoFosforo.src = "../img/fosforo.png";
+            nuevoFosforo.classList.add("fosforo"+(i%5+1));
+            this.cuentaElement.appendChild(nuevoFosforo);
+            
+        }
     }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,9 @@ import {Contador} from "./contador.js";
 
 const modal = document.querySelector("dialog");
 
+console.log(document.getElementById("jugador1-container")); // ¿es null o es un div?
+
+
 const p1 = new Contador("Nosotros",document.getElementById("jugador1-container"));
 const p2 = new Contador("Ellos", document.getElementById("jugador2-container"));
 
