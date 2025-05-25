1f32b361716b7166b41e2bbd000c1c1eeb8c7184
Tatiana Mezzano <134722879+TatianaMezzano@users.noreply.github.com>
2025-05-25 17:46:01 -0300

    ultimos detalles y estilos

--- scripts/contador.js
+++ scripts/contador.js
@@ -1,53 +1,55 @@
-export class Contador{
+export class Contador {
     numero = 0;
     nombre;
     cuentaElement;
     containerElement;
 
-    constructor(name, containerElement, cuentaInicial = 0){
+    constructor(name, containerElement, cuentaInicial = 0) {
         this.nombre = name;
         this.numero = cuentaInicial;
         this.containerElement = containerElement;
+
         containerElement.querySelector("h2").innerText = name;
-        containerElement.querySelector(".agregar").addEventListener("click",()=>this.agregar());
-        containerElement.querySelector(".restar").addEventListener("click",()=>this.restar());
+        containerElement.querySelector(".agregar").addEventListener("click", () => this.agregar());
+        containerElement.querySelector(".restar").addEventListener("click", () => this.restar());
+
         this.cuentaElement = containerElement.querySelector(".cuenta");
+        this.actualizarCuenta();
     }
 
-    agregar(cantidad = 1){
+    agregar(cantidad = 1) {
         this.numero += cantidad;
         console.log(this.numero);
         this.actualizarCuenta();
     }
 
-    restar(cantidad = 1){
-        this.numero = Math.max(0,this.numero - cantidad);
+    restar(cantidad = 1) {
+        this.numero = Math.max(0, this.numero - cantidad);
         console.log(this.numero);
         this.actualizarCuenta();
     }
 
-    reset(){
+    reset() {
         this.numero = 0;
         this.actualizarCuenta();
     }
 
-    actualizarCuenta(){
-        const separadoresActuales = this.containerElement.querySelectorAll(".separador");        
+    actualizarCuenta() {
+        const separadoresActuales = this.containerElement.querySelectorAll(".separador");
         const gruposActuales = this.containerElement.querySelectorAll(".grupo");
-        if (gruposActuales.length > 0){
-            gruposActuales.forEach(grupo => this.cuentaElement.removeChild(grupo))
-            separadoresActuales.forEach(separador => this.cuentaElement.removeChild(separador))
 
+        if (gruposActuales.length > 0) {
+            gruposActuales.forEach(grupo => this.cuentaElement.removeChild(grupo));
+            separadoresActuales.forEach(separador => this.cuentaElement.removeChild(separador));
         }
 
         let grupoActual;
-        for (let i = 0; i<this.numero;i++) {
-            if (i%5 === 0){
+        for (let i = 0; i < this.numero; i++) {
+            if (i % 5 === 0) {
                 grupoActual = document.createElement("div");
                 grupoActual.classList.add("grupo");
-                
 
-                if (i%15 === 0 && i!== 0){
+                if (i % 15 === 0 && i !== 0) {
                     const separador = document.createElement("div");
                     separador.classList.add("separador");
                     this.cuentaElement.appendChild(separador);
@@ -55,13 +57,11 @@ export class Contador{
 
                 this.cuentaElement.appendChild(grupoActual);
             }
-            
+
             const nuevoFosforo = document.createElement("img");
             nuevoFosforo.src = "../img/fosforo.png";
-            nuevoFosforo.classList.add("fosforo"+(i%5+1));
+            nuevoFosforo.classList.add("fosforo" + (i % 5 + 1));
             grupoActual.appendChild(nuevoFosforo);
-            
-            
         }
     }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,19 @@ import {Contador} from "./contador.js";
 
 const modal = document.querySelector("dialog");
 
-console.log(document.getElementById("jugador1-container")); // ¿es null o es un div?
+const p1Nombre = "Nosotros";
+const p2Nombre = "Ellos";
 
+const partidaEnMemoria = JSON.parse(localStorage.getItem("truco")) || {
+    [p1Nombre]: 0,
+    [p2Nombre]: 0,
+};
 
-const p1 = new Contador("Nosotros",document.getElementById("jugador1-container"));
-const p2 = new Contador("Ellos", document.getElementById("jugador2-container"));
+const partidap1 = partidaEnMemoria[p1Nombre];
+const partidap2 = partidaEnMemoria[p2Nombre];
+
+const p1 = new Contador(p1Nombre,document.getElementById("jugador1-container"),partidap1);
+const p2 = new Contador(p2Nombre, document.getElementById("jugador2-container"),partidap2);
 
 function reset(){
     p1.reset();
@@ -17,7 +25,18 @@ document.getElementById("reset").addEventListener("click",()=>modal.showModal())
 document.getElementById("volver").addEventListener("click",()=>modal.close())
 document.getElementById("aceptar").addEventListener("click",()=>{
     reset();
-    modal.close()
+    modal.close();
+})
+
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click",()=>{
+    const partidaAGuardar = {
+        [p1.nombre] : p1.numero,
+        [p2.nombre] : p2.numero,
+    }
+    localStorage.setItem("truco",JSON.stringify(partidaAGuardar));
+    })
+   
 })
 
 
